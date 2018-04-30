4db2236eb831000a9c36720621d853cb1ba35cbd
Infoobjects <infoobjects@Akshay-MacBook.local>
2018-04-30 21:23:13 +0530

    New Changes

--- web/app/assets/angular/js/controllers/ServiceCtrl.js
+++ web/app/assets/angular/js/controllers/ServiceCtrl.js
@@ -16,5 +16,25 @@ var ServiceCtrl = GMApp.controller('ServiceCtrl', ['$scope', '$rootScope', '$sta
         {name : "Psychometric Testing", color : "#da8616"},
         {name : "Energik Healing", color : "#ba8712"},
     ];
+    $scope.onlineCourses = [
+        {name : "Whatsappinar on Love and Relationship", color : "#bc8f12"},
+        {name : "Whatsappinar on Power of Subconscious Mind", color : "#000"},
+        {name : "Whatsappinar on Mindful Parenting", color : "#00ad91"},
+    ];
+    $scope.selfCourses = [
+        {name : "Relationship: The Soulful Loving", color : "#da4800"},
+        {name : "Spiritual Awakening", color : "#000"},
+        {name : "Spiritual Mentoring", color : "#00ad91"},
+        {name : "Spiritual Tourism (Inbound & Outbound)", color : "#412f8d"},
+        {name : "Ancestral Healing through Mind Sciences", color : "#bc8f12"},
+    ];
+    $scope.professionalCourses = [
+        {name : "Certificate Course in Counseling with Internship", color : "#4a4a4a"},
+        {name : "Master Trainer- Certificate Course in Counseling with Internship", color : "#da8616"},
+        {name : "Certificate Course in Competency Mapping with Internship (Project Based)", color : "#da4800"},
+        {name : "Internship in Clinical Hypnotherapy", color : "#00ad91"},
+    ];
+    
+
                
 }]);
