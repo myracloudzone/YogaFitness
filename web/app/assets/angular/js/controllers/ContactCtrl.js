1bdb57f7ee9df1c7d7a2a2f31b065c6e3b8d38d4
Infoobjects <infoobjects@Akshay-MacBook.local>
2018-05-08 23:52:26 +0530

    New Server

--- web/app/assets/angular/js/controllers/ContactCtrl.js
+++ web/app/assets/angular/js/controllers/ContactCtrl.js
@@ -1,4 +1,34 @@
-var ContactCtrl = GMApp.controller('ContactCtrl', ['$scope', '$rootScope', '$stateParams', '$filter', '$state','$location', function($scope, $rootScope, $stateParams,  $filter, $state, $location){
+var ContactCtrl = GMApp.controller('ContactCtrl', ['$scope', '$rootScope', '$stateParams', '$filter', '$state','$location', '$http', 'notificationService', function($scope, $rootScope, $stateParams,  $filter, $state, $location, $http, notificationService){
+    $scope.message = {to : "contact@mindandspiritualsciences.org"};
     
+    $scope.sendEmail = function() {
+        if($scope.message.name == null || $scope.message.name == '') {
+            notificationService.error("Name is required.");
+            return;
+        }
+        if($scope.message.email == null || $scope.message.email == '') {
+            notificationService.error("Email is required.");
+            return;
+        }
+        if($scope.message.subject == null || $scope.message.subject == '') {
+            notificationService.error("Subject is required.");
+            return;
+        }
+        if($scope.message.message == null || $scope.message.message == '') {
+            notificationService.error("Message is required.");
+            return;
+        }
+        $http({
+            url: 'https://grownixindia.com/service/common/sendEmail',
+            method: "POST",
+            data: $scope.message,
+            headers: {'Content-Type': 'application/json'}
+        }).success(function (data, status, headers, config) {
+            $scope.message = {to : "contact@mindandspiritualsciences.org"};
+            notificationService.success("Your request has been submitted successfully. We will reach out to you very soon.!!")
+        }).error(function (data, status, headers, config) {
+            //$scope.status = status;
+        });
+    }   
                
 }]);
